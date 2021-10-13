1b190f2456db7a4b1cc2226533cd1fd74bb843f7
thedon122 <donovan122@yahoo.com>
2021-10-12 23:39:45 -0400

    update

--- src/components/App.js
+++ src/components/App.js
@@ -23,9 +23,6 @@ const App = () => {
         setSelectedVideo(response.data.items[0]);
     };
 
-    const onVideoSelect = video => {
-        setSelectedVideo(video);
-    };
 
     return (
         <div className="ui container">
@@ -37,7 +34,7 @@ const App = () => {
                     </div>
                     <div className="five wide column">
                         <VideoList 
-                        onVideoSelect={onVideoSelect} 
+                        onVideoSelect={setSelectedVideo} 
                         videos={videos} 
                         />
                     </div>
